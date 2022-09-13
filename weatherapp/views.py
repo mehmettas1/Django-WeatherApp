f1cc954cb021a12440f5b68c1e40fd69bc34da1a
mehmet <tas349873@gmail.com>
2022-09-13 21:38:26 +0300

    update

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -3,18 +3,24 @@ from decouple import config
 import requests
 from pprint import pprint
 from django.contrib import messages
+from .models import City
 
 def index(request):
     API_KEY = config("API_KEY")
     city = "Kars"
     u_city=request.POST.get("name")
     if u_city:
-           url =f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+           url =f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_KEY}&units=metric"
            response = requests.get(url)
            print(response.ok)
            
            if response.ok:
-               pass
+               content.response.json()
+               r_city = content["name"]
+               if City.objects.filter(name=r_city):
+                    messages.warning(request,"City already exists")
+               else:
+                  City.objects.create(name=r_city)
            else:
                messages.warning(request,"There in no city")
             

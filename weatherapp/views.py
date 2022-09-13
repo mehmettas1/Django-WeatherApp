00ffa513dd9385d3943bbbb5940041643a38fd27
mehmet <tas349873@gmail.com>
2022-09-13 20:55:03 +0300

    update

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -5,7 +5,7 @@ from pprint import pprint
 
 def index(request):
     API_KEY = config("API_KEY")
-    city = "Ankara"
+    city = "Kars"
     url =f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
     response = requests.get(url)
     #? We converted the incoming data to python dictionary format 👇
@@ -17,8 +17,14 @@ def index(request):
     pprint(content["weather"][0]["description"])
     pprint(content["weather"][0]["icon"])
     
-    context={
-       "city":content["name"],
+    context = {
+        'city': content['name'],
+        'temp': content['main']['temp'],
+        'icon': content['weather'][0]['icon'],
+        'desc':content['weather'][0]['description'],
+    
     }
 
-    return render(request, 'weatherapp/index.html')
+    
+    return render(request, 'weatherapp/index.html',context)
+  
